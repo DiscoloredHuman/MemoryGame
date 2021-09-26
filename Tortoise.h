6165b56a4b7484f51ff089532f4bc118948107c5
Sylvia <70728989+DiscoloredHuman@users.noreply.github.com>
2021-09-26 17:34:13 -0400

    Update Tortoise.h

--- Tortoise.h
+++ Tortoise.h
@@ -1,8 +1,4 @@
-//google "pragma once" to see what directive means.
 #pragma once
-
-//google "include guard" or "header guard" to see
-//what the following ifndef/define/endif means.
 //motivation: avoid to include a header file more than once
 //in a project.
 #ifndef TORTOISE_H_ 
